0453c0f4967368f0c5e24a1a86a376d10e505078
Evan Pierce <mrthinger@gmail.com>
2021-07-13 19:20:00 -0400

    create offline progress

---
+++ OfflineProgress/ModEntry.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineProgress
+{
+    public class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+        }
+
+    }
+}
